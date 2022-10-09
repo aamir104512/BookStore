33abc21a042b9e875d515881e4c47b6e71ccc975
Aamir <aamir104512@gmail.com>
2022-10-09 12:52:04 +0500

    setting program structure

---
+++ constants/icons.js
@@ -0,0 +1,31 @@
+export const backArrow = require('../assets/icons/backArrow.png');
+export const bookmark = require('../assets/icons/bookmark.png');
+export const claim_icon = require('../assets/claim_icon.png');
+export const clock_icon = require('../assets/clock_icon.png');
+export const dashboard_icon = require('../assets/dashboard_icon.png');
+export const menu_icon = require('../assets/menu_icon.png');
+export const more_icon = require('../assets/more_icon.png');
+export const notification_icon = require('../assets/notification_icon.png');
+export const page_filled_icon = require('../assets/page_filled_icon.png');
+export const page_icon = require('../assets/page_icon.png');
+export const plus_icon = require('../assets/plus_icon.png');
+export const point_icon = require('../assets/point_icon.png');
+export const read_icon = require('../assets/read_icon.png');
+export const search_icon = require('../assets/search_icon.png');
+
+export default {
+  backArrow,
+  bookmark,
+  claim_icon,
+  clock_icon,
+  dashboard_icon,
+  menu_icon,
+  more_icon,
+  notification_icon,
+  page_filled_icon,
+  page_icon,
+  plus_icon,
+  point_icon,
+  read_icon,
+  search_icon,
+};
