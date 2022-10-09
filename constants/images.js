1b32b7c95bf74f6f85af87a0eb2daa5c725f88ad
Aamir <aamir104512@gmail.com>
2022-10-09 13:02:14 +0500

    modifying constants

---
+++ constants/images.js
@@ -0,0 +1,11 @@
+export const other_words_for_home = require('../assets/images/other_words_for_me.jpg');
+export const the_metropolist = require('../assets/images/the_metropolist.jpg');
+export const the_tiny_dragon = require('../assets/images/the_tiny_dragon.jpg');
+export const underland = require('../assets/images/underland.jpg');
+
+export default {
+  other_words_for_home,
+  the_metropolist,
+  the_tiny_dragon,
+  underland,
+};
