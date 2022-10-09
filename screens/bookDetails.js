1159df105aa006fe2a4bb0d6f7f16698457d77b5
Aamir <aamir104512@gmail.com>
2022-10-09 13:04:34 +0500

    home and details screens

---
+++ screens/bookDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {View, Text} from 'react-native';
+
+const bookDetails = () => {
+  return(
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontSize: 40, color: 'white'}}>This is bookDetails</Text>
+    </View>
+  )
+}
+
+export default bookDetails
